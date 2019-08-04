1ac213cbda4261271cbe28cd415ded8cb8efed80
Isabella Hannah <isabellahannah735@gmail.com>
2019-08-04 14:05:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
-//define functions here
-
-$(document).ready(function(){
-
-// call functions here
-
+function getIt(){
+$('p').on("click", function(){
+    window.alert("Hey!");
 });
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $("img").addClass("tasty");
+});
+}
+
+/* Define a function pressIt that does not accept a parameter. The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key. */
+
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+  
+  function 
+}
